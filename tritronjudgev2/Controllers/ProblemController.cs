388a88721bbd532c2ef98e0291c75d8d3eefc0d6
fahim <fahimabrar13@gmail.com>
2019-07-11 18:52:12 +0600

    ProblemController.cs added

---
+++ tritronjudgev2/Controllers/ProblemController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using tritronAPI.Persist;
+
+namespace tritronAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProblemController : ControllerBase
+    {
+        private IMapper _mapper;
+        private IUnitOfWork _uow;
+
+        public ProblemController(IMapper mapper,IUnitOfWork uow)
+        {
+            this._mapper = mapper;
+            this._uow = uow;
+        }
+        // GET: api/Problem
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Problem/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Problem
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Problem/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
