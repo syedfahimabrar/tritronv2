c022a2599dc2bc13638a5d18bac17f66594a2cde
fahim <fahimabrar13@gmail.com>
2019-07-19 12:44:06 +0600

    error logger configured

--- tritronjudgev2/Program.cs
+++ tritronjudgev2/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace tritronAPI
 {
@@ -18,7 +19,15 @@ namespace tritronAPI
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            WebHost.CreateDefaultBuilder(args).
+                ConfigureLogging((hostingContext, logging) =>
+                {
+                    // Remove all the default logging providers
+                    logging.ClearProviders();
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    // Add NLog as the Logging Provider
+                    logging.AddNLog();
+                })
                 .UseStartup<Startup>();
     }
 }
